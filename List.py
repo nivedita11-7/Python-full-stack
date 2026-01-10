88fcfffcc64ce4ed785d327587ab2c670be486d4
nivedita11-7 <niveditamalaji.views@gmail.com>
2026-01-10 10:20:46 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,16 @@
+employee=["Somanth","Raghav","Ravi"]
+print(employee)
+print(employee[0])
+print(employee[-1])
+print(employee[-3:-1])
+employee.append("Ramesh")
+employee.insert(1,"Vedanth")
+employee.remove("Ramesh")
+employee.pop()
+print(employee)
+employee.sort()
+print(employee)
+len(employee)
+
+
+    
