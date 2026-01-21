5a2f9e245da10fb7675a5a45f91e2f8ee8fb9006
nivedita11-7 <niveditamalaji.views@gmail.com>
2026-01-21 18:18:55 +0530

    Add files via upload

---
+++ grandparent.py
@@ -0,0 +1,16 @@
+class Grandparent:
+    def show_gp(self):
+        print("I am Grandparent")
+
+class Parent(Grandparent):
+    def show_parent(self):
+        print("I am parent")
+
+class Child(Parent):
+    def show_child(self):
+        print("I am child")
+
+c= Child()
+c.show_gp()
+c.show_parent()
+c.show_child()
